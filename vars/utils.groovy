eef8b433a4741b79945d869ab99b308fdc14b27e
Philip Lartey <82602322+PhilipL1@users.noreply.github.com>
2021-10-21 15:31:44 +0100

    Update utils.groovy

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -7,12 +7,3 @@ def replaceString() {
   text = text.replaceAll("%BUILD_NUMBER%", "${BUILD_NUMBER}")
   writeFile file: "index.html", text: text
 }
-
-def checkString() {
-  def text = readFile file: "index.html"
-  if(text.contain("${BUILD_NUMBER}")) {
-    echo "IT IS THERE"
-  }
-  else {
-      echo "NOT THERE"
-    }
